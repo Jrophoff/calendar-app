99b638dc7c65c4b0397975db3c580ceb551fe905
Jordan Ophoff <noreply>
2021-11-05 00:03:16 -0400

    time and date stamp js and time rows and columns html

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// (function($) {
+//     $(document),ready(function(){
+
+    
+let timeStamp = moment().format("LLLL");
+console.log(timeStamp);
+
+jQuery(".time-stamp").html(timeStamp);
+
+
+// });
+// })(jQuery);
